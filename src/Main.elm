e6e85933c25a499bc563ad08f9a555dbac6d0284
Danilo Silva <danilo_silva@ethercity.tech>
2020-12-07 04:32:36 -0300

    new preview

--- src/Main.elm
+++ src/Main.elm
@@ -4,24 +4,6 @@ import Browser
 import Http
 import Html.Events exposing (onInput, onClick)
 import Utils exposing (nothingIfEmpty, newState)
-<<<<<<< HEAD
-
-
-main : Program () Model Msg
-main =
-    Browser.element
-        { init = init
-        , view = view
-        , update = update
-        , subscriptions = \_ -> Sub.none
-        }
-
-type alias PokeResult = 
-    { name : String
-    , element : String
-    , stats   : String
-    }
-=======
 import Html.Attributes exposing (class, placeholder, type_)
 import Html exposing (Html, div, h1, p, text, input, a, i, span, ul, li)
 
@@ -35,7 +17,7 @@ main =
         , update = update
         , subscriptions = \_ -> Sub.none
         }
->>>>>>> 55aa0b26a105fc2ca8473514af3552c76bdc523a
+
 
 
 type alias Model =
@@ -54,9 +36,6 @@ init _ = ({q = Nothing, pokemonList = []}, requestPokemonList ApiPokeList)
 
 
 
-init : a -> (Model, Cmd Msg)
-init _ = ({q = Nothing, searchState = Loaded (Err "No results")}, Cmd.none)
-
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model = case msg of
 
@@ -69,14 +48,7 @@ update msg model = case msg of
                 |> String.replace " " "-"
                 |> nothingIfEmpty
         in
-<<<<<<< HEAD
             newState { model | q = maybeQ } Cmd.none
-    
-    Search  -> newState { model | searchState = Loading } Cmd.none
-
-
-=======
-            newState { model | q = updatedQ } Cmd.none
 
     Search  -> newState { model | pokemonList = [] } (requestPokemonList ApiPokeList)
 
@@ -90,18 +62,13 @@ update msg model = case msg of
 
 
 
->>>>>>> 55aa0b26a105fc2ca8473514af3552c76bdc523a
 view : Model -> Html Msg
 view model =
     div [class "container"]
         [ viewPageHeader
-<<<<<<< HEAD
-        , viewPokeResults model.q
-=======
         , viewSearchBar
         , text <| mirror model.q
         , viewPokeResults model
->>>>>>> 55aa0b26a105fc2ca8473514af3552c76bdc523a
         ]
 
 mirror mx = case mx of
@@ -133,16 +100,8 @@ viewSearchBar =
         ]
 
 
-<<<<<<< HEAD
-viewPokeResults : Maybe String -> Html Msg
-viewPokeResults str = case str of
-    Just q  -> text q
-=======
+
 viewPokeResults : Model -> Html Msg
 viewPokeResults model = ul [] <| List.map (\pk -> li [] [text pk.name]) (List.filter (\pk -> String.startsWith (mirror model.q) pk.name) model.pokemonList )
 
-
->>>>>>> 55aa0b26a105fc2ca8473514af3552c76bdc523a
-
-    Nothing -> text "No Result"
 
